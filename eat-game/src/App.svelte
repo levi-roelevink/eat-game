d5858d6db538bfc0f84b81b6004b7580352d7cb7
Levi Roelevink <538253@student.saxion.nl>
2025-12-11 22:08:39 +0100

    first commit

---
+++ eat-game/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { innerHeight, innerWidth } from "svelte/reactivity/window";
+  import image from "./assets/svelte.svg";
+  let img: HTMLImageElement | undefined = $state();
+
+  const height = $state(innerHeight.current);
+  const width = $state(innerWidth.current);
+
+  let posX = $state(height / 2);
+  let posY = $state(width / 2);``
+</script>
+
+<main id="main">
+  <img src={image} style={`top: ${posX}px; left: ${posY}px;`} />
+</main>
+
+<style>
+  main {
+    height: 100vh;
+    width: 100vw;
+  }
+
+  img {
+    width: full;
+    height: full;
+    position: absolute;
+  }
+</style>
