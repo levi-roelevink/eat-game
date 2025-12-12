4b9efeb3f0f47118b397908b7a37b997ce952e80
Levi Roelevink <538253@student.saxion.nl>
2025-12-12 11:56:25 +0100

    Little icon that moves at an interval towards a specified direction

--- eat-game/src/App.svelte
+++ eat-game/src/App.svelte
@@ -1,28 +1,84 @@
 <script lang="ts">
-    import { innerHeight, innerWidth } from "svelte/reactivity/window";
+  import { innerHeight, innerWidth } from "svelte/reactivity/window";
   import image from "./assets/svelte.svg";
-  let img: HTMLImageElement | undefined = $state();
+  import { onDestroy } from "svelte";
 
+  type Coordinate = {
+    x: number;
+    y: number;
+  };
+
+  const NORTH = "NORTH";
+  const EAST = "EAST";
+  const SOUTH = "SOUTH";
+  const WEST = "WEST";
   const height = $state(innerHeight.current);
   const width = $state(innerWidth.current);
 
-  let posX = $state(height / 2);
-  let posY = $state(width / 2);``
+  let img: HTMLImageElement | undefined = $state();
+  let curDirection: string = $state(EAST);
+  let posX = $state(width / 2);
+  let posY = $state(height / 2);
+  let ongoingGame: boolean = $state(true);
+  let score: number = 0;
+
+  function calculateNewPos(x: number, y: number, direction: string): Coordinate {
+    let coordinates: Coordinate;
+
+    switch (direction) {
+      case NORTH:
+        coordinates = { x, y: y + 10 };
+        break;
+      case EAST:
+        coordinates = { x: x + 10, y };
+        break;
+      case SOUTH:
+        coordinates = { x, y: y - 10 };
+        break;
+      case WEST:
+        coordinates = { x: x - 10, y };
+        break;
+      default:
+        throw new Error("Unknown direction used to calculate new position.");
+    }
+
+    return coordinates;
+  }
+
+  function move(): void {
+    score++;
+    const { x, y } = calculateNewPos(posX, posY, curDirection);
+    console.log(`x: ${x}, y: ${y}`);
+    posX = x;
+    posY = y;
+  }
+
+  const moveInterval: number = setInterval(move, 1000);
+  console.log("Move started.");
+
+  onDestroy(() => {
+    clearInterval(moveInterval);
+    console.log("Interval stopped on destroy.");
+  });
 </script>
 
 <main id="main">
-  <img src={image} style={`top: ${posX}px; left: ${posY}px;`} />
+  <img src={image} style={`top: ${posY}px; left: ${posX}px;`} />
 </main>
 
 <style>
-  main {
-    height: 100vh;
-    width: 100vw;
-  }
-
   img {
     width: full;
     height: full;
     position: absolute;
+  }
+
+  main {
+    height: 100vh;
+    width: 100vw;
+    position: absolute;
+    top: 0;
+    left: 0;
+    overflow: hidden;
   }
 </style>
